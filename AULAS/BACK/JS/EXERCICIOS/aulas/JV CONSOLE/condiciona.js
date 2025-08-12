5831bbd399563012f4e5ac07dac1352a580e325d
Benio <benioesteveaqui@gmail.com>
2025-08-12 09:53:24 -0300

    uify

---
+++ AULAS/BACK/JS/EXERCICIOS/aulas/JV CONSOLE/condiciona.js
@@ -0,0 +1,64 @@
+/*
+and = &&
+or = ||
+not = !
+*/
+
+// && RETORNA TRUE SE TODAS FOREM TRUE
+/*const temDinheiro = true;
+const estaSol = false;
+const tenhoTempo = true;
+const sair = temDinheiro && estaSol && tenhoTempo;
+console.log(sair)*/
+
+//  || RETORNA TRUE SE PELO MENOS UMA CONDIÇÃO FOR TRUE
+/*const temDinheiro = true;
+const estaSol = false;
+const sair = temDinheiro || estaSol;
+console.log(sair)*/
+
+// ! INVERTE O BOOLEANO
+/*const estaChovendo = false;
+console.log(!estaChovendo);*/
+
+// DUPLA NEGAÇÃO - CONVERSÃO DE DADOS PARA BOOLEANOS
+console.log(!!"texto")// STRING NÃO VAZIA = TRUES
+console.log(!!"")// STRING VAZIA = FALSE
+console.log(!!0)// ZERO RETORNA FALSE
+console.log(!!2201)// QUALQUE NÚMERO != DE 0 = TRUE
+console.log(!!undefined)// FALSE
+
+// PRIORIDADE DE OPERADORES
+const reZero = true || false && false;
+console.log(reZero); // true
+
+const re1 = (true || false) && false;
+console.log(re1); // false
+
+const a = true;
+const b = false;
+
+const re0 = !a && a || b && (!a || !b) && b
+console.log(re0)
+
+// ex preatico
+const user = "benio";
+const senha = "10";
+
+const userD = "benio";
+const senhaD = "10"
+
+const logado = userD === user && senhaD === senha;
+console.log(logado)
+
+// OP TERNÁRIO
+const pontosU = 999
+
+if (pontosU < 1000){
+    console.log("USER NORMAL")
+} else{
+    console.log("USER VIP")
+}
+
+const nivel = pontosU < 1000 ? "USER NORMAL":"USER VIP"
+console.log(nivel)
--- morole.js
+++ morole.js
@@ -1,18 +1,27 @@
 const aleatorio = (x,y) => Math.random() * (x - y) + x
 function escolha(n){
-    if (n == 1) {
-        return "ala, 1"
+    if (n === 1) {
+        return "EU VOU DIRIGIR DEPOIS DE BEBER, O VEADINHO ALI SÓ SABE CORRER, SE MULTAR Ñ VOU ME IMPORTAR, VOU ATROPELAR!!!!!"
     }
-    if(n == 2){
-        return "2 bestas programando"
+    if(n === 2){
+        return "vida de caipira não é fácil"
     }
     else{
-        return "3"
+        return "eu sinto o shadow no meu cu"
     }
 }
 
-// const a2 = aleatorio(1,3)
+
+
+
+
+
+
+
+const a2 = aleatorio(1,3)
 
 // console.log(a2)
 
-console.log(escolha(Math.round(aleatorio(1,3))))
+// console.log(escolha(Math.round(aleatorio(1,3))))
+
+console.log(escolha(Math.round(a2)))
