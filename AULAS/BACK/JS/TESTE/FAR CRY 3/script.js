75fda2f70bcb48db7d6023be9dc3f24152868254
Benio <benioesteveaqui@gmail.com>
2025-08-28 10:13:34 -0300

    UI

--- AULAS/BACK/JS/TESTE/FAR CRY 3/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("togle-theme");
-
-button.addEventListener("click", () => {
-  const isDarkMode = document.body.classList.toggle("dark-mode");
-
-  button.textContent = isDarkMode ? "Modo Claro" : "Modo Escuro";
-});
