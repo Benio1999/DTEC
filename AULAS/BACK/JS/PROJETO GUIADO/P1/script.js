beb7f56fda6a6d73a16d49f21c69ebdd6214fc61
Benio <benioesteveaqui@gmail.com>
2025-09-05 10:54:12 -0300

    shidou

--- AULAS/BACK/JS/PROJETO GUIADO/P1/script.js
+++ AULAS/BACK/JS/PROJETO GUIADO/P1/script.js
@@ -2,6 +2,13 @@ const paisIpnut = document.getElementById('paisInput');
 const buscarBtn = document.getElementById('buscarBtn');
 const container = document.getElementById('container');
 
+document.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        buscarBtn.click()
+    }
+})
+
 buscarBtn.addEventListener('click', () => {
     const nomePais = paisInput.value.trim();
     if (nomePais === '') {
@@ -9,20 +16,34 @@ buscarBtn.addEventListener('click', () => {
         return;
     }
 
-    const url = `https://restcountries.com/v3.1/name/${nomePais}`
+    const url = `https://restcountries.com/v3.1/translation/${nomePais}`
 
     fetch(url)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        const pais = data[0]
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("País Não Encontrado")
+            }
+            return response.json();
+        })
+        .then(data => {
+            const pais = data[0]
+            const moeda = Object.values(pais.currencies)[0].name;
 
-        container.innerHTML = `
-        <h2>${pais.name.common}</h2>
-        <img src="${pais.flags.svg}" alt="Bandeira do ${pais.name.common}" width="150">
-        
-        `
-    })
+            container.innerHTML = `
+                <h2>${pais.translations.por.common}</h2>
+                <img src="${pais.flags.svg}" alt="Bandeira do ${pais.name.common}" width="150">
+                <p><strong>Capital:</strong> ${pais.capital[0]}</p>
+                <p><strong>População:</strong> ${pais.population.toLocaleString()}</p>
+                <p><strong>Moeda:</strong> ${moeda}</p>
+                `
+
+            paisIpnut.value = "";
+
+        })
+        .catch(error => {
+            console.error(error)
+            container.innerHTML = `<p style="color: red;">${error.message}</p>`
+            paisIpnut.value = ""
+        })
 
 })
