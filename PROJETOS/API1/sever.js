5c8cf19324dec03a19c8e97c08ed2352f272b204
Benio <benioesteveaqui@gmail.com>
2025-09-10 11:19:18 -0300

    panela

--- PROJETOS/API1/sever.js
+++ PROJETOS/API1/sever.js
@@ -13,7 +13,8 @@ const PORT = 3001;
 let usuarios = [
     {id: 1, nome: "Ana", idade: 25},
     {id: 2, nome: "Carlos", idade: 60},
-    {id: 3, nome: "Mario", idade: 500}
+    {id: 3, nome: "Mario", idade: 500},
+    {id: 4, nome: "Mario Luigi", idade: 5}
 ]
  
 app.get('/',(req,res) => {
@@ -30,7 +31,17 @@ app.get('/usuarios/:id',(req,res) => {
     if(usuario){
         res.json(usuario)
     }else {
-        res.status(404).json(mensagem: "Usuário não encontrado")
+        res.status(404).json({mensagem: "Usuário não encontrado"})
+    }
+})
+
+app.get('/usuarios/buscar/:nome',(req,res) => {
+    const buscarNome = req.params.nome.toLowerCase()
+    const resultado = usuarios.filter(u => u.nome.toLowerCase().includes(buscarNome))
+    if(resultado.length > 0){
+        res.json(resultado)
+    }else{
+        res.status(404).json({mensagem: "Usuário não encontrado"})
     }
 })
 
