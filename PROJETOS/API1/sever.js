5474aa4b62f502db597a9ad1c40b3e5d253b7fb3
Benio <benioesteveaqui@gmail.com>
2025-09-10 10:15:39 -0300

    requiem

--- PROJETOS/API1/sever.js
+++ PROJETOS/API1/sever.js
@@ -15,13 +15,23 @@ let usuarios = [
     {id: 2, nome: "Carlos", idade: 60},
     {id: 3, nome: "Mario", idade: 500}
 ]
-
+ 
 app.get('/',(req,res) => {
     res.send("PÁGINA INICIAL")
 })
 
 app.get('/usuarios',(req,res) => {
     res.json(usuarios);
+})
+
+app.get('/usuarios/:id',(req,res) => {
+    const id = req.params.id
+    const usuario = usuarios.find(u => u.id == id)
+    if(usuario){
+        res.json(usuario)
+    }else {
+        res.status(404).json(mensagem: "Usuário não encontrado")
+    }
 })
 
 //Inicia o servidor
