65428714efa6c013789adaac169ef8639d71e7a8
Benio <benioesteveaqui@gmail.com>
2025-09-10 08:34:56 -0300

    meu pai amado

--- AULAS/BACK/JS/PROJETO GUIADO/P2/script.js
+++ AULAS/BACK/JS/PROJETO GUIADO/P2/script.js
@@ -1,58 +1,80 @@
-const cidadeIpnut = document.getElementById('cidadeInput');
-const buscarBtn = document.getElementById('buscarBtn');
-const container = document.getElementById('container');
+const cidadeInput = document.getElementById("cidadeInput");
+const buscarBtn = document.getElementById("buscarBtn");
+const container = document.getElementById("container");
+
 
 document.addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
         event.preventDefault()
         buscarBtn.click()
+
     }
 })
 
 buscarBtn.addEventListener('click', () => {
-    const nomeCidade = cidadeIpnut.value.trim();
-    if (nomeCidade === '') {
-        alert('por favor, digite o nome de uma cidade')
+    const nomeCidade = cidadeInput.value.trim();
+    if (nomeCidade === "") {
+        alert("Por favor, digite o nome de uma cidade")
         return;
     }
 
     const url = `https://nominatim.openstreetmap.org/search?q=${nomeCidade}&format=jsonv2`
 
-    container.innerHTML = `<p>Buscando...</p>`
 
     fetch(url)
         .then(response => {
             if (!response.ok) {
-                throw new Error("Lamento irmão, mas cidade não encontrada")
+                throw new Error("Country not found!!!")
             }
             return response.json();
         })
 
         .then(data => {
-            const cidades = data[0];
-            const lat = cidades.lat;
-            const lon = cidades.lon;
-            const url2 = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}}&current=temperature_2m,is_day`
+            const cidade = data[0]
+            const lat = cidade.lat;
+            const lon = cidade.lon;
+
+            const url2 = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,is_day`
+
             fetch(url2)
                 .then(response => {
                     return response.json();
                 })
+
                 .then(data => {
-                    const meteo = data;
+                    const local = data;
+                    const is_day = local.current.is_day;
+                    let displayDay = ""
+
+                    if (is_day) {
+                        displayDay = "dia";
+                        backgroundColor = "#7ccfff";
+                        cor = "#000000ff"
+                    }
+                    else {
+                        displayDay = "noite";
+                        backgroundColor = "rgba(78, 11, 159, 1)";
+                        cor = "#ffffffff"
+                    }
+
+                    container.style.backgroundColor = backgroundColor;
+                    container.style.color = cor;
 
                     container.innerHTML = `
-                <h2>${cidades.display_name}</h2>
-                <p>${cidades.lat.toLocaleString()}</p>
-                <p>${meteo.current.temperature_2m}</p>
-                <p>${meteo.current.is_day}</p>
-                `
-                    cidadeIpnut.value = "";
+        <p><strong>Cidade:</strong> ${cidade.name} </p>
+        <h2>${local.current.temperature_2m} °C</h2>
+        <p>${displayDay} </p>
+
+        `
+
                 })
+            cidadeInput.value = "";
         })
         .catch(error => {
             console.error(error)
-            container.innerHTML = `<p style="color: red;">${error.message}</p>`
-            cidadeIpnut.value = ""
+            container.innerHTML = `<p style="color: red;">Erro: ${error.message}</p>`
+            cidadeInput.value = "";
         })
+
 
 })
---
+++ PROJETOS/API1/sever.js
@@ -0,0 +1,27 @@
+//importanto express
+const express = require('express');
+
+//criando minha aplicação
+const app = express();
+
+//permitir trabalhar com json
+app.use(express.json());
+
+//porta onde a API vai rodar
+const PORT = 3001;
+
+let usuarios = [
+    {id: 1, nome: "Ana", idade: 25},
+    {id: 2, nome: "Carlos", idade: 60},
+    {id: 3, nome: "Mario", idade: 500}
+]
+
+
+app.get('/usuarios',(req,res) => {
+    res.json(usuarios);
+})
+
+//Inicia o servidor
+app.listen(PORT, () =>{
+    console.log(`Servidor na porta ${PORT}`)
+})
