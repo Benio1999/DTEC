fee1c5f8278e9f81601e0552df9809fd7fcc7d3e
Benio <benioesteveaqui@gmail.com>
2025-09-11 11:21:36 -0300

    ez

--- PROJETOS/API1/sever.js
+++ PROJETOS/API1/sever.js
@@ -11,41 +11,80 @@ app.use(express.json());
 const PORT = 3001;
 
 let usuarios = [
-    {id: 1, nome: "Ana", idade: 25},
-    {id: 2, nome: "Carlos", idade: 60},
-    {id: 3, nome: "Mario", idade: 500},
-    {id: 4, nome: "Mario Luigi", idade: 5}
+    { id: 1, nome: "Ana", idade: 25 },
+    { id: 2, nome: "Carlos", idade: 60 },
+    { id: 3, nome: "Mario", idade: 500 },
+    { id: 4, nome: "Mario Luigi", idade: 25 }
 ]
- 
-app.get('/',(req,res) => {
+
+app.get('/', (req, res) => {
     res.send("PÁGINA INICIAL")
 })
 
-app.get('/usuarios',(req,res) => {
+app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 })
 
-app.get('/usuarios/:id',(req,res) => {
-    const id = req.params.id
-    const usuario = usuarios.find(u => u.id == id)
-    if(usuario){
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    const usuario = usuarios.find(u => u.id == id);
+    if (usuario) {
         res.json(usuario)
-    }else {
-        res.status(404).json({mensagem: "Usuário não encontrado"})
+    } else {
+        res.status(404).json({ mensagem: "Usuário não encontrado" })
     }
 })
 
-app.get('/usuarios/buscar/:nome',(req,res) => {
-    const buscarNome = req.params.nome.toLowerCase()
-    const resultado = usuarios.filter(u => u.nome.toLowerCase().includes(buscarNome))
-    if(resultado.length > 0){
+app.get('/usuarios/nome/:nome', (req, res) => {
+    const buscarNome = req.params.nome.toLowerCase();
+    const resultado = usuarios.filter(u => u.nome.toLowerCase().includes(buscarNome));
+    if (resultado.length > 0) {
         res.json(resultado)
-    }else{
-        res.status(404).json({mensagem: "Usuário não encontrado"})
+    } else {
+        res.status(404).json({ mensagem: "Usuário não encontrado" })
+    }
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    usuarios = usuarios.filter(u => u.id != id)
+    res.json({ mensagem: "Usuário removido com sucesso!" })
+})
+
+app.post("/usuarios", (req, res) => {
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: req.body.nome,
+        idade: req.body.idade
+    };
+    usuarios.push(novoUsuario)
+    res.status(201).json(usuarios)
+})
+
+app.put('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const idade = req.body.idade;
+    const usuario = usuarios.find(u => u.id == id);
+    if (!usuario) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" })
+    }
+    usuario.nome = nome || usuario.nome
+    usuario.idade = idade || usuario.idade
+    res.json(usuario)
+})
+
+app.get('/usuarios/idade/:idade', (req, res) => {
+    const buscaIdade = req.params.idade;
+    const resultado = usuarios.filter(u => u.idade == buscaIdade);
+    if (resultado.length > 0) {
+        res.json(resultado)
+    } else {
+        res.status(404).json({ mensagem: "Usuário não encontrado" })
     }
 })
 
 //Inicia o servidor
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Servidor na porta ${PORT}`)
 })
