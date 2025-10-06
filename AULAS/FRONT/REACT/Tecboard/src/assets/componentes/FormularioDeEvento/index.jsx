ddaf0c88c9a08adf424c18282e61cc982e9fd71c
Benio <benioesteveaqui@gmail.com>
2025-10-06 11:24:52 -0300

    aula react

--- AULAS/FRONT/REACT/Tecboard/src/assets/componentes/FormularioDeEvento/index.jsx
+++ AULAS/FRONT/REACT/Tecboard/src/assets/componentes/FormularioDeEvento/index.jsx
@@ -1,20 +1,39 @@
 import '../FormularioDeEvento/FormularioDeEvento.css'
+import { Botao } from '../Botão';
+import { CampoDeEntrada } from '../CampoDeEntrada';
 import { CampoDeFormulario } from '../CampoDeFormulario';
 import { Label } from "../Label";
+import { ListaSuspensa } from '../ListaSuspensa';
 import { TituloFormulario } from "../TituloFormulario";
-import { CampoDeEntrada } from '../CampoDeEntrada';
-
 
 export function FormularioDeEvento() {
     return (
         <form className='form-evento'>
             <TituloFormulario> Preencha para criar um evento: </TituloFormulario>
 
+            <div className='campos'>
+                <CampoDeFormulario>
+                    <Label htmlFor="nome">Qual é o nome do evento: </Label>
+                    <CampoDeEntrada type="text" id="nome" placeholder='Sumer dev hits' />
+                </CampoDeFormulario>
 
-            <CampoDeFormulario>
-                <Label htmlFor="nome">Qual é o nome do evento: </Label>
-                <CampoDeEntrada type="text" id="nome" placeholder='Sumer dev hits' />
-            </CampoDeFormulario>
+                <CampoDeFormulario>
+                    <Label htmlFor="dataEvento">Qual é a data do evento: </Label>
+                    <CampoDeEntrada type="date" id="nome" placeholder='Data do Evento' />
+                </CampoDeFormulario>
+
+                <CampoDeFormulario>
+                    <Label htmlFor="dataEvento">Qual é o tipo do evento: </Label>
+                    <ListaSuspensa />
+                </CampoDeFormulario>
+            </div>
+
+            <div className='acoes'>
+                <Botao>
+                    Criar evento
+                </Botao>
+            </div>
+
         </form>
     )
 }
