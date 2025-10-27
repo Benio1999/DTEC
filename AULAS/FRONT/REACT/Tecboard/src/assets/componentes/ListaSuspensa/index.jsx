5360ce8e5257f12f2a445ef5e0e3039afd6d361f
Benio <benioesteveaqui@gmail.com>
2025-10-27 11:06:11 -0300

    show

--- AULAS/FRONT/REACT/Tecboard/src/assets/componentes/ListaSuspensa/index.jsx
+++ AULAS/FRONT/REACT/Tecboard/src/assets/componentes/ListaSuspensa/index.jsx
@@ -2,7 +2,7 @@ import './ListaSuspensa.css'
 
 export function ListaSuspensa({itens, ...rest}) {
     return (
-        <select defaultValue="" className='lista-suspensa-form'>
+        <select defaultValue="" className='lista-suspensa-form' {...rest}>
             <option disabled value=''>Selecione uma opção</option>
             {itens.map(function (item) {
                 return (
